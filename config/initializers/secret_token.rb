1f2efb5f23a37c25300d2d604cc6228308e7553c
Drackenov <Drackenov@gmail.com>
2013-08-17 16:00:22 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hogs::Application.config.secret_key_base = '5fa4501a4c0faa2b77db79e8d36ade167878477a8939f2b91614cadf1d7b19bf6ef97d00914649564298dca4065ea9e8a9acb4b8aaaeb98b3f4844828a8f358b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hogs::Application.config.session_store :cookie_store, key: '_hogs_session'
